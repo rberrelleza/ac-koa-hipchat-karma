e07e3985c7f6d852276f3d793fcc75202ab82f27
Bob Bergman <rbergman@atlassian.com>
2014-09-24 09:55:08 -0600

    Refactor with allowRoom with group-wide storage

--- web.js
+++ web.js
@@ -1,31 +1,36 @@
 var analytics = require('ac-koa-hipchat-keenio');
+var MongoStore = require('ac-node').MongoStore;
+var Karma = require('./lib/karma');
 
 var ack = require('ac-koa').require('hipchat');
 var pkg = require('./package.json');
-var app = ack(pkg, {store: 'MongoStore'});
+var app = ack(pkg, {store: MongoStore});
 
 var addon = app.addon()
   .hipchat()
   .allowGlobal(true)
+  .allowRoom(true)
   .scopes('send_notification', 'view_group');
 
+var addonStore = MongoStore(addon.config.mongoEnv, 'karma');
 var tracker = analytics.track(addon);
 
 addon.webhook('room_message', /^\/karma(?:\s+(:)?(.+?)\s*$)?/i, function *() {
   var message;
   var match = this.match;
   var room = this.room;
-  var enabled = (yield this.tenantStore.get(roomKey(room.id))) !== false;
+  var karma = Karma(addonStore, this.tenant.group);
+  var enabled = yield karma.isEnabled(room.id);
   var command = match && match[1] === ':' && match[2];
   var subject = match && !match[1] && match[2];
   if (command) {
     if (command === 'on') {
       enabled = true;
-      yield this.tenantStore.set(roomKey(room.id), enabled);
+      yield karma.setEnabled(room.id, enabled);
       message = 'Karma matching has been enabled in this room.';
     } else if (command === 'off') {
       enabled = false;
-      yield this.tenantStore.set(roomKey(room.id), enabled);
+      yield karma.setEnabled(room.id, enabled);
       message = 'Karma matching has been disabled in this room.';
     } else {
       message = 'Sorry, I didn\'t understand that.';
@@ -33,28 +38,29 @@ addon.webhook('room_message', /^\/karma(?:\s+(:)?(.+?)\s*$)?/i, function *() {
   } else if (subject) {
     if (subject.charAt(0) === '@') {
       var user = yield this.tenantClient.getUser(subject);
-      var karma;
+      var value;
       if (user) {
         subject = user.name;
-        karma = (yield getSubject('user', user.id)) || 0;
+        value = karma.forUser(user.id);
       } else {
-        karma = (yield getSubject('thing', subject)) || 0;
+        value = karma.forThing(subject);
       }
     } else {
-      karma = (yield getSubject('thing', subject)) || 0;
+      value = karma.forThing(subject);
     }
-    message = subject + ' has ' + karma + ' karma.';
+    message = subject + ' has ' + value + ' karma.';
   } else {
     message =
       '<pre>' +
       'Karma matching is currently ' + (enabled ? 'enabled' : 'disabled') + ' in this room.\n\n' +
       'Usage:\n' +
       '  /karma                 print this help message\n' +
-      '  /karma :on             enable karma matching in the current room\n' +
-      '  /karma :off            disable karma matching in the current room\n' +
       '  /karma {thing}         lookup something\'s current karma\n' +
       '  /karma @{MentionName}  lookup a user\'s current karma\n' +
+      '  /karma :on             enable karma matching in the current room\n' +
+      '  /karma :off            disable karma matching in the current room\n' +
       '  {subject}++            add karma to a given subject\n' +
+      '  {subject}++++          add 3 karma to a given subject\n' +
       '  {subject}--            remove karma to a given subject\n' +
       '  "{subject phrase}"++   add karma to a given subject phrase\n' +
       '  @{MentionName}++       add karma to a given user by mention name\n' +
@@ -70,8 +76,10 @@ addon.webhook('room_message', new RegExp(strIncDec), function *() {
   // don't parse other slash commands
   if (/^\/\w+/.test(this.content)) return;
   // don't respond if disabled in the current room
-  if ((yield this.tenantStore.get(roomKey(room.id))) === false) return;
+  var store = addonStore.narrow(this.tenant.group);
+  if ((yield store.get(roomKey(room.id))) === false) return;
 
+  var karma = Karma(addonStore, this.tenant.group);
   var reIncDec = new RegExp(strIncDec, 'g');
   var match;
   while (match = reIncDec.exec(this.content)) {
@@ -80,7 +88,7 @@ addon.webhook('room_message', new RegExp(strIncDec), function *() {
     var isMention = match[1] && match[1].charAt(0) === '@';
     var change = match[6].length - 1;
     var changed = (change > 0 ? 'increased' : 'decreased');
-    var karma;
+    var value;
     if (match[6].charAt(0) === '-') {
       change = -change;
     }
@@ -91,22 +99,22 @@ addon.webhook('room_message', new RegExp(strIncDec), function *() {
       });
       if (user) {
         if (user.id === sender.id) {
-          notify.call(this, 'Don\'t be a weasle.');
+          notify.call(this, 'Don\'t be a weasel.');
           return;
         } else {
           subject = user.name;
-          karma = yield updateSubject('user', user.id, change);
+          value = yield karma.updateUser(user.id, change);
         }
       } else {
         subject = match[1];
-        karma = yield updateSubject('thing', subject, change);
+        value = karma.updateThing(subject, change);
       }
     } else {
       subject = match[2] || match[3] || match[5];
-      karma = yield updateSubject('thing', subject, change);
+      value = karma.updateThing(subject, change);
     }
     var possessive = subject + '\'' + (subject.charAt(subject.length - 1) === 's' ? '' : 's');
-    message = possessive + ' karma has ' + changed + ' to ' + karma + '.';
+    message = possessive + ' karma has ' + changed + ' to ' + value + '.';
     notify.call(this, message);
   }
 });
@@ -115,28 +123,4 @@ app.listen();
 
 function notify(message) {
   this.roomClient.sendNotification(message, {color: 'gray', format: 'html'});
-}
-
-function updateSubject(type, subject, change) {
-  return function *() {
-    var key = subjectKey(type, subject);
-    var karma = ((yield this.tenantStore.get(key)) || 0) + change;
-    yield this.tenantStore.set(key, karma);
-    return karma;
-  };
-}
-
-function getSubject(type, subject) {
-  return function *() {
-    var key = subjectKey(type, subject);
-    return yield this.tenantStore.get(key);
-  };
-}
-
-function subjectKey(type, subject) {
-  return type + ':' + subject.toString().toLowerCase().trim();
-}
-
-function roomKey(roomId) {
-  return 'room:' + roomId;
 }
